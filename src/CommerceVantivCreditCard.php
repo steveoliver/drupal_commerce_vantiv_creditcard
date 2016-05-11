87c766eb55b6f13eca287048e7502167cefdf876
Steve Oliver <steve@circatree.com>
2016-05-11 13:32:32 -0500

    Add CommerceVantivCreditCard class (placeholder).

---
+++ src/CommerceVantivCreditCard.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\commerce_vantiv_creditcard\CommerceVantivCreditCard;
+ */
+namespace Drupal\commerce_vantiv_creditcard;
+/**
+ * Implements the CommerceVantivCreditCard class.
+ */
+class CommerceVantivCreditCard {
+
+  /**
+   * Test function to return 'foo'.
+   *
+   * @return string
+   *   Returns 'foo'.
+   */
+  public function bar() {
+    return 'foo';
+  }
+}
