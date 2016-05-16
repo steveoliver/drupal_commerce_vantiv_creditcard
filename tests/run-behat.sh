898dc7bbcbbe4f3084057e4b414afb85b2468e5d
Steve Oliver <steve@circatree.com>
2016-05-16 13:36:32 -0700

    Use Mink Debug Extension (https://github.com/Lakion/MinkDebugExtension).

--- tests/run-behat.sh
+++ tests/run-behat.sh
@@ -8,4 +8,7 @@ DIR=$(dirname $0)
 cd $DIR
 cd ./behat
 test -f "./vendor/bin/behat" || composer install --no-interaction --prefer-source --dev
+mkdir logs
 ./vendor/bin/behat "$@"
+
+./vendor/bin/upload-textfiles "logs/*.log"
