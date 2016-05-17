6c53fa24267d43746ae7309ab3f0091496e7e088
Steve Oliver <steve@circatree.com>
2016-05-17 15:06:50 -0700

    Move back to pwd in before and after scripts.

--- tests/drupal_ti/after/environments/drupal-8.sh
+++ tests/drupal_ti/after/environments/drupal-8.sh
@@ -3,7 +3,9 @@
 # Performs actions after a Drupal-8 environment build.
 
 # Upload Behat Mink Debug Extension logs.
+CWD = $(pwd -P)
 cd $TRAVIS_BUILD_DIR
 cd $DRUPAL_TI_BEHAT_DIR
 
 ./vendor/bin/upload-textfiles "logs/*.log"
+cd $CWD
--- tests/drupal_ti/before/environments/drupal-8.sh
+++ tests/drupal_ti/before/environments/drupal-8.sh
@@ -9,5 +9,10 @@ export DRUPAL_TI_SIMPLETEST_GROUP="commerce_vantiv_creditcard_group";
 export DRUPAL_TI_INSTALL_PROFILE="standard";
 
 # Make a place for Behat Mink Debug Extension logs.
+CWD = $(pwd -P)
 cd $TRAVIS_BUILD_DIR
-mkdir $DRUPAL_TI_BEHAT_DIR/logs
+if ![ -d $DRUPAL_TI_BEHAT_DIR/logs ]
+then
+  mkdir $DRUPAL_TI_BEHAT_DIR/logs
+fi
+cd $CWD
