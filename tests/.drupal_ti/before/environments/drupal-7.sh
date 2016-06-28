6c5197f61a029fecc9d0c91870d06076bd232935
Steve Oliver <steve@circatree.com>
2016-06-27 23:33:08 -0700

    Smash it all in the environment script.

--- tests/.drupal_ti/before/environments/drupal-7.sh
+++ tests/.drupal_ti/before/environments/drupal-7.sh
@@ -3,3 +3,34 @@
 echo "MODULES_DIR: $DRUPAL_TI_DRUPAL_DIR/$DRUPAL_TI_MODULES_PATH"
 
 echo "SCRIPT_DIR: $DRUPAL_TI_SCRIPT_DIR"
+
+# Move to sites/all.
+cd "$DRUPAL_TI_DRUPAL_DIR/DRUPAL_TI_MODULES_PATH/../"
+
+# Make libraries directory.
+mkdir libraries
+
+# Download library dependencies.
+git clone --depth 1 https://github.com/steveoliver/vantiv_devhub_sdk_php.git libraries/vantiv-devhub-php
+
+# Download module dependencies.
+mkdir modules
+cd modules
+drush dl \
+  addressfield-7.x-1.2 \
+  ctools-7.x-1.9 \
+  entity-7.x-.7 \
+  libraries-7.x-2.2 \
+  rules-7.x-2.9 \
+  views-7.x-3.14 \
+  commerce-7.x-1.13 \
+  jquery_update-7.x-3.x \
+  commerce_cardonfile-7.x-2.x \
+  commerce_recurring-7.x-2.x \
+  interval-7.x-1.0
+
+# Go to Drupal root directory.
+cd "$DRUPAL_TI_DRUPAL_DIR/$DRUPAL_TI_MODULES_PATH"
+
+# Enable hard dependencies
+drush en -y rules
