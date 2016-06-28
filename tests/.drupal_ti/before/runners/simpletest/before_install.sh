0f78388210a95018d94a4d1dd5e093eaf66d1adf
Steve Oliver <steve@circatree.com>
2016-06-28 00:06:14 -0700

    Maybe now?

--- tests/.drupal_ti/before/runners/simpletest/before_install.sh
+++ tests/.drupal_ti/before/runners/simpletest/before_install.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
-echo "BEFORE INSTALL"
-echo "DRUPAL DIR: $DRUPAL_TI_DRUPAL_DIR"
+# Move to sites/all.
+cd "$DRUPAL_TI_DRUPAL_DIR/$DRUPAL_TI_MODULES_PATH/../"
+
+# Make libraries directory.
+mkdir -p libraries
+
+# Download library dependencies.
+git clone --depth 1 https://github.com/steveoliver/vantiv_devhub_sdk_php.git libraries/vantiv-devhub-php
+
+# Download module dependencies.
+mkdir -p modules
+cd modules
+drush dl --yes \
+  addressfield-7.x-1.2 \
+  ctools-7.x-1.9 \
+  entity-7.x-1.7 \
+  libraries-7.x-2.2 \
+  rules-7.x-2.9 \
+  views-7.x-3.14 \
+  commerce-7.x-1.13 \
+  jquery_update-7.x-3.x \
+  commerce_cardonfile-7.x-2.x \
+  commerce_recurring-7.x-2.x \
+  interval-7.x-1.0
 
